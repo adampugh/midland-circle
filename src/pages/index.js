b4a251b590d7635d31bb862f21e76cc015407a6c
Adam <adampugh@zoho.com>
2019-05-01 16:06:48 +0100

    Fetched blog posts on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,43 @@
-import React from "react"
+import React, { Component } from "react"
 import Layout from '../components/Layout';
+import { Link } from 'react-router-dom';
 
 // fetch blog data from markdown files
 
-const Home = () => (
-    <Layout>
-        <h1>hello</h1>
-        <button className="btn btn-primary">click this</button>
-    </Layout>
-);
+
+class Home extends Component {
+    render() {
+        const { edges } = this.props.data.allMarkdownRemark
+
+        return (
+            <Layout>
+                { console.log(edges) }
+                <h1>hello</h1>
+                <button className="btn btn-primary">click this</button>
+                { edges.map(edge => <div>
+                    <Link to={edge.node.frontmatter.path}>{edge.node.frontmatter.title}</Link>
+                    </div>)}
+            </Layout>
+        )
+    }
+}
+
+export const pageQuery = graphql`
+query blogPosts {
+    allMarkdownRemark {
+      edges {
+        node {
+          id,
+          frontmatter {
+            title,
+            thumbnail,
+            path
+          }
+        }
+      }
+    } 
+  }
+`;
+
 
 export default Home;
